a34e0c8c662d7891054dd5a699370d2e39c9beae
idrozhil <idrozhil@example.com>
2023-06-01 14:51:24 +0300

    Initial commit

---
+++ src/main/java/io/datajek/spring/dbmanytomany/DbManyToManyApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.dbmanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbManyToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbManyToManyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/datajek/spring/dbmanytomany/DbManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.dbmanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbManyToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
